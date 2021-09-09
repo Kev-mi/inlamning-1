77116018c15643b0a39d66212acb7ebed805326d
KevinMina <90063807+KevinMina@users.noreply.github.com>
2021-09-09 20:10:38 +0200

    Update inlamning 2.py

--- inlamning 2.py
+++ inlamning 2.py
@@ -17,10 +17,10 @@ class DataHandler:
 url = 'https://github.com/NordAxon/ec-python-course/blob/master/assignments/01_inlamningsuppgift_2_data.csv?raw=true'
 df = pd.read_csv(url)
 
-vaccinated_per_day_mean = df.groupby(['country'])[['daily_vaccinations']].mean()
-
-parser.add_argument('--country1', help="enter first country", required=True, type=str)
-parser.add_argument('--country2', help="enter second country", required=True, type=str)
-parser.add_argument('--data-flag', help="enter what you want the program to do", required=True, type=str)
+vaccinated_per_day_mean = df.groupby(['country'])[['daily_vaccinations',"daily_vaccinations_per_million"]].mean()
+print(vaccinated_per_day_mean)
+#parser.add_argument('--country1', help="enter first country", required=True, type=str)
+#parser.add_argument('--country2', help="enter second country", required=True, type=str)
+#parser.add_argument('--data-flag', help="enter what you want the program to do", required=True, type=str)
 
 
