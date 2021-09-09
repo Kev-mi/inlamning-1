2b98cad84a18ddf3d8613689abf074b01aad203b
KevinMina <90063807+KevinMina@users.noreply.github.com>
2021-09-09 09:29:21 +0200

    Update Inlämning 1.py

--- Inlämning 1.py
+++ Inlämning 1.py
@@ -1,97 +1,104 @@
 import random
 
-def Number_guessing_game(Correct_Number: int) -> int:
+
+def number_guessing_game(correct_number: int) -> int:
     '''Function that asks the user to guess the correct random number
-    
+
     Args:
-        Correct_Number (int): the randomly generated number that the user is asked to guess
-    
+        correct_number(int): the randomly generated number that the user is asked to guess
+
     Returns:
-        Number_Of_guesses: integer that points to number of guesses
-    
+        number_of_guesses: integer that points to number of guesses
+
     '''
 
-    Number_Of_Guesses = 1
-    Random_Number = random.randint(1, 10)
-    while Correct_Number == False:
+    number_of_guesses = 1
+    random_number = random.randint(1, 10)
+    while correct_number == False:
         try:
-            Guessed_Number = int(input("Guess a random number between 1 and 10"))
-            if Guessed_Number == Random_Number:
+            guessed_number = int(input("Guess a random number between 1 and 10"))
+            if guessed_number == random_number:
                 print("The number is correct")
-                print("It took you " + str(Number_Of_Guesses) + " tries")
-                return Number_Of_Guesses
+                print("It took you " + str(number_of_guesses) + " tries")
+                return number_of_guesses
             else:
-                Number_Of_Guesses += 1
-                if Guessed_Number > Random_Number:
+                number_of_guesses += 1
+                if guessed_number > random_number:
                     print("The number is too large")
                 else:
                     print("The number is too small")
         except ValueError:
             print("That was not a number!")
 
-def Score_Appender(Score: int) -> None:
+
+def score_appender(score: int) -> None:
     '''Function that takes the amount of guesses as result and adds to a .txt file
-    
+
     Args:
-        Score (int): the number of guesses it took the user to answer correctly
-    
+        score (int): the number of guesses it took the user to answer correctly
+
     Returns:
         None
-        
+
     '''
 
-    Highscore = open("Highscore_Guesses.txt", "a")
-    Highscore.write(" %s" % str(Score))
-    Highscore.close()
+    highscore = open("highscore_guesses.txt", "a")
+    highscore.write(" %s" % str(score))
+    highscore.close()
     return None
 
-def Number_Guessing_Loop(Want_To_Play: int) -> None:
+
+def number_guessing_loop(want_to_play: int) -> None:
     '''Function that asks if the user wants to continue to play.
-    
+
     Args:
         Want_To_Play (int): int that determines if user wants to play
-            
+
     Returns:
         None:
-            
+
      '''
 
-    while Want_To_Play == 1:
-        Number_Of_Guesses = Number_guessing_game(False)
-        Score_Appender(Number_Of_Guesses)
-        Want_To_Play = int(input("Type 1 if you want to play again or 0 if you want to quit"))
-        if Want_To_Play == 0:
+    want_to_play = 1
+
+    while want_to_play == 1:
+        number_of_guesses = number_guessing_game(False)
+        score_appender(number_of_guesses)
+        want_to_play = int(input("Type 1 if you want to play again or 0 if you want to quit"))
+        if want_to_play == 0:
             return None
     return None
 
-def Highscores():
+
+def highscores():
     '''The function if the user wants to see highscores of guesses.
-    
+
     Args:
         None
-        
+
     Returns:
         None
-        
+
     '''
 
-    Want_To_See = int(input("Type 1 if you want to see highscores, otherwise the program will terminate"))
-    if Want_To_See == 1:
-        File = open("Highscore_Guesses.txt", "r")
-        Guesses_Highscores = File.read()
-        Guesses_Highscores = Guesses_Highscores.split()
-        for x in range(0, len(Guesses_Highscores)):
-            Guesses_Highscores[x] = int(Guesses_Highscores[x])
-        Guesses_Highscores.sort()
-        print(Guesses_Highscores)
-        File.close()
+    want_to_see = int(input("Type 1 if you want to see highscores, otherwise the program will terminate"))
+    if want_to_see == 1:
+        file = open("Highscore_Guesses.txt", "r")
+        guesses_highscores = file.read()
+        guesses_highscores = guesses_highscores.split()
+        for x in range(0, len(guesses_highscores)):
+            guesses_highscores[x] = int(guesses_highscores[x])
+        guesses_highscores.sort()
+        print(guesses_highscores)
+        file.close()
         return None
     else:
         return None
 
-User_Name = input("What is your name?")
-print("Hello " + User_Name)
 
-Number_Guessing_Loop(1)
+user_name = input("What is your name?")
+print("Hello " + user_name)
 
-Highscores()
+number_guessing_loop()
+
+highscores()
