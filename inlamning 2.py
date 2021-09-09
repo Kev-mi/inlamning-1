9b5b740a95b26a68f22b6d8524336bd38561f5a7
KevinMina <90063807+KevinMina@users.noreply.github.com>
2021-09-09 13:58:55 +0200

    Create inlamning 2.py

---
+++ inlamning 2.py
@@ -0,0 +1,5 @@
+import pandas as pd
+import numpy as np
+
+#pokemon_df = pd.read_csv('Pokemon.csv')
+url = 'https://github.com/NordAxon/ec-python-course/blob/master/assignments/01_inlamningsuppgift_2_data.csv?raw=true'
